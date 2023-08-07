5248094e8df361fc13224b76a837dcfc3eb44612
jchaselubitz <jchaselubitz@gmail.com>
2023-08-07 12:33:29 -0400

    fix upload creds

--- pages/api/file/upload.ts
+++ pages/api/file/upload.ts
@@ -6,7 +6,7 @@ const keyFilePath = path.join(process.cwd(), '/syndicate-cloud-key-staging.json'
 
 export const upload = multer({
   storage: new MulterGoogleStorage({
-    projectId: process.env.NEXT_PRIVATE_GOOGLE_CLOUD_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_PROJECT_ID,
     bucket: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_BUCKET,
     keyFilename: keyFilePath,
     public: true,
